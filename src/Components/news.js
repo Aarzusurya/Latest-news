50380953c912d9186139b003af518f3cb79663fd
TechAarzu <suryakritu@gmail.com>
2026-04-17 19:43:40 +0530

    Update news API endpoint to use environment variable

--- src/Components/news.js
+++ src/Components/news.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const response = await fetch(
-    `https://newsapi.org/v2/everything?q=india&apiKey=YOUR_API_KEY`
+    `https://newsapi.org/v2/everything?q=${q}&apiKey=${process.env.NEWS_API_KEY}`
   );
   const data = await response.json();
   res.status(200).json(data);
