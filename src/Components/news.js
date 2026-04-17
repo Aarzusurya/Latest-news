52d18533785243e589439d4d695202ff5356a7d8
TechAarzu <suryakritu@gmail.com>
2026-04-17 19:55:36 +0530

    Delete src/Components/news.js

--- src/Components/news.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch(
-    `https://newsapi.org/v2/everything?q=${q}&apiKey=${process.env.NEWS_API_KEY}`
-  );
-  const data = await response.json();
-  res.status(200).json(data);
-}
